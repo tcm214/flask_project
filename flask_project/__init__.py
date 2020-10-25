a2f3d18e11ae23d35234cdc322b3f58768032af3
Tyler Mueller <tcm214@gmail.com>
2020-10-25 11:29:11 -0600

    reset password functionality

--- flask_project/__init__.py
+++ flask_project/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 
 
-#################################### ep 9 10min
+
+#################################### ep 10 16min
 
 
 app = Flask(__name__)
@@ -20,6 +23,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+#app.config[''] = 'yhgmiyigzjsjqkrc'
+mail = Mail(app)
+
+
 
 from flask_project import routes
 
